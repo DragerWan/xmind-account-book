4f487f39d8c4b7c424d5febd4b097618c440138d
DragerWan <dragerwan@163.com>
2020-05-09 19:00:27 +0800

    中文加载乱码

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import XLSX from 'xlsx';
 
 const useStyles = makeStyles({
   table: {
@@ -14,20 +16,70 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name:string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+function getBills(setBills: (bills:any) => any) {
+    fetch('/bill.csv')
+        .then(function(response) {
+            return response.arrayBuffer();
+        })
+        .then(function(respAB) {
+            let workbook = XLSX.read(new Uint8Array(respAB), {type: 'array'});
+            let sheet:object[] = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+            console.log("sheet", sheet);
+            let first:any = sheet[0];
+            Object.keys(first).forEach((key)=>{
+                console.log(key, first[key])
+            });
+            setBills(sheet);
+        }).catch(e=>{
+          console.log("error: ", e);
+    });
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+function getCategories(setCategories: (categories:any) => any) {
+    fetch('/categories.csv')
+        .then(function(response) {
+            return response.arrayBuffer();
+        })
+        .then(function(respAB) {
+            let workbook = XLSX.read(new Uint8Array(respAB), {type: 'array'});
+            let sheet:object[] = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+            let first:any = sheet[0];
+            Object.keys(first).forEach((key)=>{
+                console.log(key, first[key])
+            });
+            setCategories(sheet);
+        }).catch(e=>{
+        console.log("error: ", e);
+    });
+}
 
 function MyTable() {
   const classes = useStyles();
+  const [bills, setBills] = useState();
+  const [categories, setCategories] = useState();
+  useEffect(()=>{
+      getBills(setBills);
+      getCategories(setCategories);
+  }, []);
+
+  let list = [];
+  if(bills && categories){
+      let categoryMap:any = {};
+      for(let{id, type, name} of categories){
+          categoryMap[id] = {
+              type,
+              name
+          }
+      }
+      list = bills.map((bill:any) => ({
+          type: bill.type,
+          time: bill.time,
+          category: categoryMap[bill.category].name,
+          amount: bill.amount
+      }));
+  }
+
+
   return (
     <div>
       <TableContainer component={Paper}>
@@ -38,19 +90,17 @@ function MyTable() {
             <TableCell align="right">Calories</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
+          {list.map((row:any, index:number) => (
+            <TableRow key={index}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.type}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.category}</TableCell>
+              <TableCell align="right">{row.amount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
